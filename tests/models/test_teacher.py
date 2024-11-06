167623bf003252c6cd7e1d30ba32d7b8b032d96f
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-11-06 10:46:51 +0100

    add test_add_cours in teacher class

--- tests/models/test_teacher.py
+++ tests/models/test_teacher.py
@@ -1,6 +1,8 @@
 import pytest
 from datetime import date
-from models.teacher import Teacher, Course
+from models.teacher import Teacher
+from models.course import Course
+from models.student import Student
 
 def test_create_teacher():
     teacher = Teacher(first_name="Jane", last_name= "Doe", age=30, hiring_date=date(2020, 1, 1))
@@ -9,3 +11,9 @@ def test_create_teacher():
     assert teacher.age == 30
     assert teacher.hiring_date == date(2020, 1, 1)
     assert teacher.courses_teached == []
+
+def test_add_course():
+    student = Student(first_name="John", last_name="Doe", age=20)
+    course = Course(name="Math 101", start_date=date(2024, 1, 1), end_date=date(2024, 6, 1))
+    student.add_course(course)
+    assert course in student.courses_taken
